bfc44f9132191540f95254a98986b4164d6ac3b9
Sharma <sharmaisse@gmail.com>
2024-01-09 03:57:18 -0800

    tuple with length

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    def multiple_returns(sentence):
+    result_tuple = ()
+    if len(sentence) == 0:
+        result_tuple = 0, None
+    else:
+        result_tuple = len(sentence), sentence[0]
+    return result_tuple
