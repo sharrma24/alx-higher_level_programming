1c0cdf95679a0cfcca92c2730b92a60af32f9fe7
Sharma <sharmaisse@gmail.com>
2024-01-09 03:59:17 -0800

    multiple touples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     def multiple_returns(sentence):
     result_tuple = ()
     if len(sentence) == 0:
-        result_tuple = 0, None
+        result_tuple = 0," None"
     else:
         result_tuple = len(sentence), sentence[0]
     return result_tuple
