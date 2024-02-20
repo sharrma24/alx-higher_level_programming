d7a522fbed0d96eaf4bcca79d764e74fcfe46104
Sharma <sharmaisse@gmail.com>
2024-02-20 00:54:34 -0800

    2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-"""Specifies a function for reading text files."""
+"""Describes a function for reading text files."""
 
 def read_file(filename=""):
-        """Display the content of a UTF8 text file on the standard output."""
+    """Displays the content of a UTF8 text file on the standard output.
+
+    Args:
+        filename (str): The name of the file to be read.
+    """
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end="")
+
