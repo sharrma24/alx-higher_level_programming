7467f78d4b0874132fbd41c5e90e7f9f442fd5a9
Sharma <sharmaisse@gmail.com>
2024-02-20 00:44:38 -0800

    1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
+"""Specifies a function for reading text files."""
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout.
-
-    Args:
-        filename (str): The name of the text file.
-
-    Returns:
-        None
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content)
-
-# Example usage:
-read_file("example.txt")
+        """Display the content of a UTF8 text file on the standard output."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
